789bdcbfece1c435514dd208c573a015a6d68277
Bibesh Manandhar <bibesh@Bibeshs-MacBook-Pro.local>
2019-02-17 09:38:53 +0545

    completed sails API

---
+++ api/controllers/ToDoTaskController.js
@@ -0,0 +1,90 @@
+/**
+ * ToDoTaskController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  task: async function(req, res) {
+    if (req.method == "GET") {
+      let id = req.param("id");
+      let todoTask = await ToDoTask.find({
+        id
+      });
+      return res.send(todoTask);
+    } else return res.send("Method mismatch error");
+  },
+
+  list: async function(req, res) {
+    let todoTasks = await ToDoTask.find();
+    return res.send(todoTasks);
+  },
+
+  add: async function(req, res) {
+    let id = req.param("id");
+    let taskName = req.param("taskName");
+    let taskDetail = req.param("taskDetail");
+    let taskDate = req.param("taskDate");
+    if (req.method == "POST") {
+      try {
+        var enterToDatabase = await ToDoTask.create({
+          id,
+          taskName,
+          taskDetail,
+          taskDate
+        }).fetch();
+        return res.send(
+          await ToDoTask.find({
+            id: enterToDatabase.id
+          })
+        );
+      } catch (err) {
+        return res.send(err);
+      }
+    } else {
+      return res.serverError();
+    }
+  },
+
+  remove: async function(req, res) {
+    let id = req.param("id");
+    if (req.method == "POST") {
+      ToDoTask.destroy({
+        id
+      }).then(function() {
+        console.log("hi");
+        return res.ok();
+      });
+    } else {
+      return res.serverError();
+    }
+  },
+
+  edit: async function(req, res) {
+    let id = req.param("id");
+    let taskName = req.param("taskName");
+    let taskDetail = req.param("taskDetail");
+    let taskDate = req.param("taskDate");
+    if (req.method == "POST") {
+      try {
+        var enterToDatabase = await ToDoTask.update(id)
+          .set({
+            taskName,
+            taskDetail,
+            taskDate
+          })
+          .fetch();
+        return res.send(
+          await ToDoTask.find({
+            id: enterToDatabase.id
+          })
+        );
+      } catch (err) {
+        return res.send(err);
+      }
+    } else {
+      return res.serverError();
+    }
+  }
+};
---
+++ config/routes.js
@@ -0,0 +1,52 @@
+/**
+ * Route Mappings
+ * (sails.config.routes)
+ *
+ * Your routes tell Sails what to do each time it receives a request.
+ *
+ * For more information on configuring custom routes, check out:
+ * https://sailsjs.com/anatomy/config/routes-js
+ */
+
+module.exports.routes = {
+  /***************************************************************************
+   *                                                                          *
+   * Make the view located at `views/homepage.ejs` your home page.            *
+   *                                                                          *
+   * (Alternatively, remove this and add an `index.html` file in your         *
+   * `assets` directory)                                                      *
+   *                                                                          *
+   ***************************************************************************/
+
+  "/": {
+    controller: "todotask",
+    action: "list"
+  },
+
+  /***************************************************************************
+   *                                                                          *
+   * More custom routes here...                                               *
+   * (See https://sailsjs.com/config/routes for examples.)                    *
+   *                                                                          *
+   * If a request to a URL doesn't match any of the routes in this file, it   *
+   * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
+   * not match any of those, it is matched against static assets.             *
+   *                                                                          *
+   ***************************************************************************/
+  "todotask/task/*": "ToDoTaskController.task",
+
+  "get todotask/list": {
+    controller: "todotask",
+    action: "list"
+  },
+  "post todotask/add/:id/:taskName/:taskDetail/:taskDate": {
+    controller: "todotask",
+    action: "/add"
+  },
+  "post todotask/remove": {
+    action: "todotask/remove"
+  },
+  "post todotask/edit": {
+    action: "todotask/edit"
+  }
+};
